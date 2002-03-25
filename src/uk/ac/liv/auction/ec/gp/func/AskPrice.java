6890f539d202ae0a5c873a6556490106792dec07
sphelps <sphelps>
2002-03-25 16:42:17 +0000

    *** empty log message ***

---
+++ src/uk/ac/liv/auction/ec/gp/func/AskPrice.java
@@ -0,0 +1,20 @@
+package uk.ac.liv.auction.ec.gp.func;
+
+import ec.gp.*;
+import ec.*;
+
+import uk.ac.liv.ec.gp.func.*;
+
+import uk.ac.liv.util.GenericDouble;
+
+
+public class AskPrice extends GPNode {
+
+  public void eval( EvolutionState state, int thread, GPData input, ADFStack stack, GPIndividual individual, Problem problem ) {
+    ((GPNumberData) input).data = new GenericDouble( new Double(((GPAuctioneer) individual).clearAsk.getPrice()) );
+  }
+
+  public String toString() {
+    return "AskPrice";
+  }
+}
