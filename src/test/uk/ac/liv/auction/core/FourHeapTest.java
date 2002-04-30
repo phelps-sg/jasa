9acf2da19d140a7b782ded8e4873387b6200beba
sphelps <sphelps>
2002-04-30 13:26:37 +0000

    *** empty log message ***

--- src/test/uk/ac/liv/auction/core/FourHeapTest.java
+++ src/test/uk/ac/liv/auction/core/FourHeapTest.java
@@ -2,14 +2,14 @@
  * JASA Java Auction Simulator API
  * Copyright (C) Steve Phelps
  *
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  */
 
@@ -88,7 +88,7 @@ public class FourHeapTest extends TestCase {
           Shout ask = (Shout) i.next();
           assertTrue( bid.isBid() );
           assertTrue( ask.isAsk() );
-          assertTrue( bid.getPrice() > ask.getPrice() );
+          assertTrue( bid.getPrice() >= ask.getPrice() );
           //System.out.print(bid + "/" + ask + " ");
         }
         //System.out.println("");
