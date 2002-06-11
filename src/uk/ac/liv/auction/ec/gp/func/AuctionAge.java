c867a88ea45ffceaef2b6ba873d3e06d1eacf04e
sphelps <sphelps>
2002-06-11 12:42:38 +0000

    *** empty log message ***

---
+++ src/uk/ac/liv/auction/ec/gp/func/AuctionAge.java
@@ -0,0 +1,35 @@
+/*
+ * JASA Java Auction Simulator API
+ * Copyright (C) 2001-2002 Steve Phelps
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ */
+
+package uk.ac.liv.auction.ec.gp.func;
+
+import ec.gp.*;
+import ec.*;
+
+import uk.ac.liv.ec.gp.func.*;
+
+import uk.ac.liv.util.GenericLong;
+
+
+public class AuctionAge extends GPNode {
+
+  public void eval( EvolutionState state, int thread, GPData input, ADFStack stack, GPIndividual individual, Problem problem ) {
+    ((GPGenericData) input).data = new GenericLong( new Long(((GPTradingStrategy) individual).getAuction().getAge()) );
+  }
+
+  public String toString() {
+    return "AuctionAge";
+  }
+}
