94f75083052f639271d85b7d2323f5e2305fe89d
sphelps <sphelps>
2002-07-01 12:40:25 +0000

    *** empty log message ***

---
+++ src/uk/ac/liv/util/MutableIntWrapper.java
@@ -0,0 +1,15 @@
+package uk.ac.liv.util;
+
+
+public class MutableIntWrapper {
+
+  public int value;
+
+  public MutableIntWrapper() {
+  }
+
+  public int intValue() {
+    return value;
+  }
+
+}
