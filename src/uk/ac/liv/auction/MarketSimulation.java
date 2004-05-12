ccd1af1a8b4fe590f53b0f4e3fd6da24091a6196
sphelps <sphelps>
2004-05-12 09:48:31 +0000

    *** empty log message ***

--- src/uk/ac/liv/auction/MarketSimulation.java
+++ src/uk/ac/liv/auction/MarketSimulation.java
@@ -77,7 +77,7 @@ public class MarketSimulation
   public static final String P_SIMULATION = "simulation";
 
 
-  public static final String VERSION = "0.30";
+  public static final String VERSION = "0.31";
 
   public static final String GNU_MESSAGE =
     "\n" +
