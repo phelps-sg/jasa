d252fff6eceb722b40ab2c7c458a6117ec687832
jzniu <jzniu>
2005-02-14 20:50:26 +0000

    uk.ac.liv.auction.agent.RandomUnconstrainedStrategy added.

---
+++ src/uk/ac/liv/auction/agent/RandomUnconstrainedStrategy.java
@@ -0,0 +1,100 @@
+/*
+ * JASA Java Auction Simulator API
+ * Copyright (C) 2001-2005 Steve Phelps
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ */
+
+package uk.ac.liv.auction.agent;
+
+import uk.ac.liv.auction.agent.AbstractTradingAgent;
+import uk.ac.liv.auction.agent.FixedQuantityStrategyImpl;
+import uk.ac.liv.auction.core.Shout;
+import uk.ac.liv.auction.core.Auction;
+
+import uk.ac.liv.prng.GlobalPRNG;
+
+import ec.util.ParameterDatabase;
+import ec.util.Parameter;
+
+import java.io.Serializable;
+
+import cern.jet.random.AbstractContinousDistribution;
+import cern.jet.random.Uniform;
+
+/**
+ * <p>
+ * A trading strategy in which an agent bid regardless its private 
+ * value.  This strategy is often referred to as Zero Intelligence
+ * Unconstrained (ZI-U) in the literature.
+ * </p>
+ *
+ * </p>
+ * <p>
+ * <b>Parameters </b> <br>
+ * <table>
+ * <tr>
+ * <td valign=top><i>base </i> <tt>.maxprice</tt><br>
+ * <font size=-1>double &gt;= 0 </font></td>
+ * <td valign=top>(max price in auction)</td>
+ * <tr>
+ * 
+ * </table>
+ *
+ * @author Jinzhong Niu
+ * @version $Revision$
+ */
+
+public class RandomUnconstrainedStrategy extends FixedQuantityStrategyImpl
+                                        implements Serializable {
+
+    public static final String P_MAXPRICE = "maxprice";
+
+    public static double MAX_PRICE = 200;
+
+    protected AbstractContinousDistribution distribution;
+
+
+  public RandomUnconstrainedStrategy() {
+    super(null);
+  }
+
+  public RandomUnconstrainedStrategy( AbstractTradingAgent agent) {
+    super(agent);
+  }
+  
+  public void setup( ParameterDatabase parameters, Parameter base ) {
+      super.setup(parameters, base);
+      MAX_PRICE = parameters.getDoubleWithDefault(base.push(P_MAXPRICE),
+                                                    null, MAX_PRICE);
+      
+      distribution = new Uniform(0, MAX_PRICE, GlobalPRNG.getInstance());      
+  }
+
+
+  public boolean modifyShout( Shout.MutableShout shout ) {
+
+    double price = distribution.nextDouble();
+    shout.setPrice(price);
+    shout.setQuantity(quantity);
+
+    return super.modifyShout(shout);
+  }
+
+  public void endOfRound( Auction auction ) {
+    // Do nothing
+  }
+
+  public String toString() {
+    return "(" + getClass() + " quantity:" + quantity + ")";
+  }
+
+}
