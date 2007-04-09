b1a559aaa1e29a791bfe8dfd632171b381d20b52
jzniu <jzniu>
2007-04-09 16:11:47 +0000

    - informBeginOfDay() added

--- src/uk/ac/liv/auction/core/AuctionImpl.java
+++ src/uk/ac/liv/auction/core/AuctionImpl.java
@@ -15,32 +15,30 @@
 
 package uk.ac.liv.auction.core;
 
-import uk.ac.liv.auction.event.AuctionEventListener;
-import uk.ac.liv.auction.event.AuctionEvent;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Observable;
+
 import uk.ac.liv.auction.event.AgentPolledEvent;
 import uk.ac.liv.auction.event.AuctionClosedEvent;
+import uk.ac.liv.auction.event.AuctionEvent;
+import uk.ac.liv.auction.event.AuctionEventListener;
 import uk.ac.liv.auction.event.AuctionOpenEvent;
+import uk.ac.liv.auction.event.DayOpeningEvent;
 import uk.ac.liv.auction.event.EndOfDayEvent;
 import uk.ac.liv.auction.event.RoundClosedEvent;
 import uk.ac.liv.auction.event.RoundClosingEvent;
 import uk.ac.liv.auction.event.ShoutPlacedEvent;
 import uk.ac.liv.auction.event.ShoutReceivedEvent;
 import uk.ac.liv.auction.event.TransactionExecutedEvent;
-
 import uk.ac.liv.auction.stats.AuctionReport;
 import uk.ac.liv.auction.stats.CombiAuctionReport;
-
 import uk.ac.liv.util.IdAllocator;
 import uk.ac.liv.util.Resetable;
-
-import java.io.Serializable;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Observable;
-import java.util.LinkedList;
-import java.util.Iterator;
 
 /**
  * An abstract implementation of Auction that provides basic logging facilities.
@@ -50,320 +48,324 @@ import java.util.Iterator;
  */
 
 public abstract class AuctionImpl extends Observable implements Auction,
-    Serializable, Resetable {
+		Serializable, Resetable {
 
-  /**
-   * The name of this auction.
-   * 
-   * @uml.property name="name"
-   */
-  protected String name;
+	/**
+	 * The name of this auction.
+	 * 
+	 * @uml.property name="name"
+	 */
+	protected String name;
 
-  /**
-   * Used to assign unique ids to each instance.
-   */
-  static IdAllocator idAllocator = new IdAllocator();
+	/**
+	 * Used to assign unique ids to each instance.
+	 */
+	static IdAllocator idAllocator = new IdAllocator();
 
-  /**
-   * A unique id for this auction. It's main use is in debugging.
-   * 
-   * @uml.property name="id"
-   */
-  protected long id;
+	/**
+	 * A unique id for this auction. It's main use is in debugging.
+	 * 
+	 * @uml.property name="id"
+	 */
+	protected long id;
 
-  /**
-   * Flag indicating whether the auction is currently closed.
-   * 
-   * @uml.property name="closed"
-   */
-  protected boolean closed;
+	/**
+	 * Flag indicating whether the auction is currently closed.
+	 * 
+	 * @uml.property name="closed"
+	 */
+	protected boolean closed;
 
-  /**
-   * The plugable auction rules to use for this auction, e.g.
-   * AscendingAuctioneer.
-   * 
-   * @uml.property name="auctioneer"
-   * @uml.associationEnd
-   */
-  protected Auctioneer auctioneer = null;
+	/**
+	 * The plugable auction rules to use for this auction, e.g.
+	 * AscendingAuctioneer.
+	 * 
+	 * @uml.property name="auctioneer"
+	 * @uml.associationEnd
+	 */
+	protected Auctioneer auctioneer = null;
 
-  /**
-   * The optional MarketDataLogger to log data to.
-   * 
-   * @uml.property name="report"
-   * @uml.associationEnd
-   */
-  protected AuctionReport report = null;
+	/**
+	 * The optional MarketDataLogger to log data to.
+	 * 
+	 * @uml.property name="report"
+	 * @uml.associationEnd
+	 */
+	protected AuctionReport report = null;
 
-  /**
-   * @uml.property name="eventListeners"
-   * @uml.associationEnd multiplicity="(0 -1)" ordering="true"
-   *                     elementType="uk.ac.liv.auction.event.AuctionEventListener"
-   *                     qualifier="eventClass:java.lang.Class java.util.List"
-   */
-  protected HashMap eventListeners = new HashMap();
+	/**
+	 * @uml.property name="eventListeners"
+	 * @uml.associationEnd multiplicity="(0 -1)" ordering="true"
+	 *                     elementType="uk.ac.liv.auction.event.AuctionEventListener"
+	 *                     qualifier="eventClass:java.lang.Class java.util.List"
+	 */
+	protected HashMap eventListeners = new HashMap();
 
-  private static final Class[] allEvents = { RoundClosedEvent.class,
+	private static final Class[] allEvents = { RoundClosedEvent.class,
 			RoundClosingEvent.class, AuctionOpenEvent.class,
 			AuctionClosedEvent.class, EndOfDayEvent.class,
 			TransactionExecutedEvent.class, ShoutPlacedEvent.class,
 			AgentPolledEvent.class, ShoutReceivedEvent.class };
 
-  public AuctionImpl( String name ) {
-    id = idAllocator.nextId();
-    if ( name != null ) {
-      this.name = name;
-    } else {
-      this.name = "Auction " + id;
-    }
-    // initialise();
-  }
+	public AuctionImpl(String name) {
+		id = idAllocator.nextId();
+		if (name != null) {
+			this.name = name;
+		} else {
+			this.name = "Auction " + id;
+		}
+		// initialise();
+	}
 
-  public AuctionImpl() {
-    this(null);
-  }
+	public AuctionImpl() {
+		this(null);
+	}
 
-  protected void initialise() {
-    closed = false;
-  }
+	protected void initialise() {
+		closed = false;
+	}
 
-  public void reset() {
-    initialise();
-    eventListeners.clear();
-  }
+	public void reset() {
+		initialise();
+		eventListeners.clear();
+	}
 
-  /**
-   * @uml.property name="auctioneer"
-   */
-  public void setAuctioneer( Auctioneer auctioneer ) {
-    this.auctioneer = auctioneer;
-    auctioneer.setAuction(this);
-  }
+	/**
+	 * @uml.property name="auctioneer"
+	 */
+	public void setAuctioneer(Auctioneer auctioneer) {
+		this.auctioneer = auctioneer;
+		auctioneer.setAuction(this);
+	}
 
-  /**
-   * @uml.property name="auctioneer"
-   */
-  public Auctioneer getAuctioneer() {
-    return auctioneer;
-  }
+	/**
+	 * @uml.property name="auctioneer"
+	 */
+	public Auctioneer getAuctioneer() {
+		return auctioneer;
+	}
 
-  public boolean closed() {
-    return closed;
-  }
+	public boolean closed() {
+		return closed;
+	}
 
-  /**
-   * Close the auction.
-   */
-  public void close() {
-    closed = true;
-  }
+	/**
+	 * Close the auction.
+	 */
+	public void close() {
+		closed = true;
+	}
 
-  /**
-   * @uml.property name="lastShout"
-   */
-  public Shout getLastShout() throws ShoutsNotVisibleException {
-//    if ( !auctioneer.shoutsVisible() ) {
-//      throw new ShoutsNotVisibleException();
-//    }
-//    return lastShout;
-    return auctioneer.getLastShout();
-  }
+	/**
+	 * @uml.property name="lastShout"
+	 */
+	public Shout getLastShout() throws ShoutsNotVisibleException {
+		// if ( !auctioneer.shoutsVisible() ) {
+		// throw new ShoutsNotVisibleException();
+		// }
+		// return lastShout;
+		return auctioneer.getLastShout();
+	}
 
-  /**
-   * Assign a data logger
-   * 
-   * @uml.property name="report"
-   */
-  public void setReport( AuctionReport logger ) {
-    this.report = logger;
-    removeAuctionEventListener(logger);
-    addAuctionEventListener(logger);
-  }
+	/**
+	 * Assign a data logger
+	 * 
+	 * @uml.property name="report"
+	 */
+	public void setReport(AuctionReport logger) {
+		this.report = logger;
+		removeAuctionEventListener(logger);
+		addAuctionEventListener(logger);
+	}
 
-  /**
-   * Get the current data logger
-   * 
-   * @uml.property name="report"
-   */
-  public AuctionReport getReport() {
-    return report;
-  }
+	/**
+	 * Get the current data logger
+	 * 
+	 * @uml.property name="report"
+	 */
+	public AuctionReport getReport() {
+		return report;
+	}
 
-  /**
-   * Change the name of this auction.
-   * 
-   * @param name
-   *          The new name of the auction.
-   * @uml.property name="name"
-   */
-  public void setName( String name ) {
-    this.name = name;
-  }
+	/**
+	 * Change the name of this auction.
+	 * 
+	 * @param name
+	 *          The new name of the auction.
+	 * @uml.property name="name"
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public MarketQuote getQuote() {
-    return auctioneer.getQuote();
-  }
+	public MarketQuote getQuote() {
+		return auctioneer.getQuote();
+	}
 
-  /**
-   * @uml.property name="name"
-   */
-  public String getName() {
-    return name;
-  }
+	/**
+	 * @uml.property name="name"
+	 */
+	public String getName() {
+		return name;
+	}
 
-  /**
-   * @uml.property name="id"
-   */
-  public long getId() {
-    return id;
-  }
+	/**
+	 * @uml.property name="id"
+	 */
+	public long getId() {
+		return id;
+	}
 
-  public void removeShout( Shout shout ) {
-    // Remove this shout and all of its children.
-    for ( Shout s = shout; s != null; s = s.getChild() ) {
-      auctioneer.removeShout(s);
-      // if ( s != shout ) {
-      // ShoutPool.release(s);
-      // }
-    }
-    shout.makeChildless();
-  }
+	public void removeShout(Shout shout) {
+		// Remove this shout and all of its children.
+		for (Shout s = shout; s != null; s = s.getChild()) {
+			auctioneer.removeShout(s);
+			// if ( s != shout ) {
+			// ShoutPool.release(s);
+			// }
+		}
+		shout.makeChildless();
+	}
 
-  /**
-   * Handle a new shout in the auction.
-   * 
-   * @param shout
-   *          The new shout in the auction.
-   */
-  public void newShout( Shout shout ) throws AuctionException {
-    if ( closed() ) {
-      throw new AuctionClosedException("Auction " + name + " is closed.");
-    }
-    if ( shout == null ) {
-      throw new IllegalShoutException("null shout");
-    }
+	/**
+	 * Handle a new shout in the auction.
+	 * 
+	 * @param shout
+	 *          The new shout in the auction.
+	 */
+	public void newShout(Shout shout) throws AuctionException {
+		if (closed()) {
+			throw new AuctionClosedException("Auction " + name + " is closed.");
+		}
+		if (shout == null) {
+			throw new IllegalShoutException("null shout");
+		}
 
-    auctioneer.newShout(shout);
+		auctioneer.newShout(shout);
 
-//    notifyObservers();
-  }
+		// notifyObservers();
+	}
 
-  public void printState() {
-    auctioneer.printState();
-  }
+	public void printState() {
+		auctioneer.printState();
+	}
 
-  /**
-   * Add a new market data logger.
-   * 
-   * @param newReport
-   *          The new logger to add.
-   */
-  public void addReport( AuctionReport newReport ) {
-    AuctionReport oldReport = report;
-    if ( !(oldReport instanceof CombiAuctionReport) ) {
-      setReport(new CombiAuctionReport());
-      if ( oldReport != null ) {
-        ((CombiAuctionReport) report).addReport(oldReport);
-      }
-    }
-    ((CombiAuctionReport) report).addReport(newReport);
-  }
+	/**
+	 * Add a new market data logger.
+	 * 
+	 * @param newReport
+	 *          The new logger to add.
+	 */
+	public void addReport(AuctionReport newReport) {
+		AuctionReport oldReport = report;
+		if (!(oldReport instanceof CombiAuctionReport)) {
+			setReport(new CombiAuctionReport());
+			if (oldReport != null) {
+				((CombiAuctionReport) report).addReport(oldReport);
+			}
+		}
+		((CombiAuctionReport) report).addReport(newReport);
+	}
 
-  public void addListener( LinkedList listeners, AuctionEventListener listener ) {
-    assert listener != null;
+	public void addListener(LinkedList listeners, AuctionEventListener listener) {
+		assert listener != null;
 
-    if ( !listeners.contains(listener) ) {
-      listeners.add(listener);
-    }
-  }
+		if (!listeners.contains(listener)) {
+			listeners.add(listener);
+		}
+	}
 
-  public void addAuctionEventListener( AuctionEventListener listener ) {
-    for ( int i = 0; i < allEvents.length; i++ ) {
-      addAuctionEventListener(allEvents[i], listener);
-    }
-  }
+	public void addAuctionEventListener(AuctionEventListener listener) {
+		for (int i = 0; i < allEvents.length; i++) {
+			addAuctionEventListener(allEvents[i], listener);
+		}
+	}
 
-  public void removeAuctionEventListener( AuctionEventListener listener ) {
-    for ( int i = 0; i < allEvents.length; i++ ) {
-      removeAuctionEventListener(allEvents[i], listener);
-    }
-  }
+	public void removeAuctionEventListener(AuctionEventListener listener) {
+		for (int i = 0; i < allEvents.length; i++) {
+			removeAuctionEventListener(allEvents[i], listener);
+		}
+	}
 
-  public void addAuctionEventListener( Class eventClass,
-      AuctionEventListener listener ) {
-    LinkedList listenerList = (LinkedList) eventListeners.get(eventClass);
-    if ( listenerList == null ) {
-      listenerList = new LinkedList();
-      eventListeners.put(eventClass, listenerList);
-    }
-    listenerList.add(listener);
-  }
+	public void addAuctionEventListener(Class eventClass,
+			AuctionEventListener listener) {
+		LinkedList listenerList = (LinkedList) eventListeners.get(eventClass);
+		if (listenerList == null) {
+			listenerList = new LinkedList();
+			eventListeners.put(eventClass, listenerList);
+		}
+		listenerList.add(listener);
+	}
 
-  public void removeAuctionEventListener( Class eventClass,
-      AuctionEventListener listener ) {
-    LinkedList listenerList = (LinkedList) eventListeners.get(eventClass);
-    if ( listenerList != null ) {
-      listenerList.remove(listener);
-    }
-  }
+	public void removeAuctionEventListener(Class eventClass,
+			AuctionEventListener listener) {
+		LinkedList listenerList = (LinkedList) eventListeners.get(eventClass);
+		if (listenerList != null) {
+			listenerList.remove(listener);
+		}
+	}
 
-  protected void fireEvent( AuctionEvent event ) {
-    List listeners = (List) eventListeners.get(event.getClass());
-    if ( listeners != null ) {
-      Iterator i = listeners.iterator();
-      while ( i.hasNext() ) {
-        AuctionEventListener listener = (AuctionEventListener) i.next();
-        listener.eventOccurred(event);
-      }
-    }
-  }
+	protected void fireEvent(AuctionEvent event) {
+		List listeners = (List) eventListeners.get(event.getClass());
+		if (listeners != null) {
+			Iterator i = listeners.iterator();
+			while (i.hasNext()) {
+				AuctionEventListener listener = (AuctionEventListener) i.next();
+				listener.eventOccurred(event);
+			}
+		}
+	}
 
-  public void informAuctionClosed() {
-    fireEvent(new AuctionClosedEvent(this, getRound()));
-  }
+	public void informAuctionClosed() {
+		fireEvent(new AuctionClosedEvent(this, getRound()));
+	}
 
-  public void informEndOfDay() {
-    fireEvent(new EndOfDayEvent(this, getRound()));
-  }
+	public void informEndOfDay() {
+		fireEvent(new EndOfDayEvent(this, getRound()));
+	}
 
-  public void informAuctionOpen() {
-    fireEvent(new AuctionOpenEvent(this, getRound()));
-  }
+	public void informBeginOfDay() {
+		fireEvent(new DayOpeningEvent(this, getRound()));
+	}
 
-  /**
-   * Return a Map of all of the variables in all of the reports configured for
-   * this auction. The Map maps report variables, represented as objects of type
-   * ReportVariable onto their values.
-   * 
-   * @see uk.ac.liv.auction.stats.ReportVariable
-   */
-  public Map getResults() {
-    if ( report != null ) {
-      return report.getVariables();
-    } else {
-      return new HashMap();
-    }
-  }
+	public void informAuctionOpen() {
+		fireEvent(new AuctionOpenEvent(this, getRound()));
+	}
 
-  public AuctionReport getReport( Class reportClass ) {
-    if ( report != null ) {
-      if ( report.getClass().equals(reportClass) ) {
-        return report;
-      } else if ( report instanceof CombiAuctionReport ) {
-        Iterator i = ((CombiAuctionReport) report).reportIterator();
-        while ( i.hasNext() ) {
-          AuctionReport report = (AuctionReport) i.next();
-          if ( report.getClass().equals(reportClass) ) {
-            return report;
-          }
-        }
-      }
-    }
-    return null;
-  }
+	/**
+	 * Return a Map of all of the variables in all of the reports configured for
+	 * this auction. The Map maps report variables, represented as objects of type
+	 * ReportVariable onto their values.
+	 * 
+	 * @see uk.ac.liv.auction.stats.ReportVariable
+	 */
+	public Map getResults() {
+		if (report != null) {
+			return report.getVariables();
+		} else {
+			return new HashMap();
+		}
+	}
 
-  public String toString() {
-    return "(Auction id:" + id + ")";
-  }
+	public AuctionReport getReport(Class reportClass) {
+		if (report != null) {
+			if (report.getClass().equals(reportClass)) {
+				return report;
+			} else if (report instanceof CombiAuctionReport) {
+				Iterator i = ((CombiAuctionReport) report).reportIterator();
+				while (i.hasNext()) {
+					AuctionReport report = (AuctionReport) i.next();
+					if (report.getClass().equals(reportClass)) {
+						return report;
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public String toString() {
+		return "(Auction id:" + id + ")";
+	}
 
 }
