b69d7fc156c47670720551986ba66e46d3cf276e
jzniu <jzniu>
2007-04-09 15:54:29 +0000

    - updated the calculation of variance; when origin is not the mean, the previous calculation is incorrect.

--- src/uk/ac/liv/util/CummulativeDistribution.java
+++ src/uk/ac/liv/util/CummulativeDistribution.java
@@ -46,204 +46,204 @@ import org.apache.log4j.Logger;
  */
 
 public class CummulativeDistribution implements Serializable, Cloneable,
-    Resetable, Distribution {
+		Resetable, Distribution {
 
-  /**
-   * The number of data in the series so far.
-   * 
-   * @uml.property name="n"
-   */
-  protected int n;
+	/**
+	 * The number of data in the series so far.
+	 * 
+	 * @uml.property name="n"
+	 */
+	protected int n;
 
-  /**
-   * The cummulative total of all numbers in the series so far.
-   * 
-   * @uml.property name="total"
-   */
-  protected double total;
+	/**
+	 * The cummulative total of all numbers in the series so far.
+	 * 
+	 * @uml.property name="total"
+	 */
+	protected double total;
 
-  /**
-   * The total of the squares of all numbers in the series so far.
-   * 
-   * @uml.property name="totalSq"
-   */
-  protected double totalSq;
+	/**
+	 * The total of the squares of all numbers in the series so far.
+	 * 
+	 * @uml.property name="totalSq"
+	 */
+	protected double totalSq;
 
-  /**
-   * The minimum so far.
-   * 
-   * @uml.property name="min"
-   */
-  protected double min;
+	/**
+	 * The minimum so far.
+	 * 
+	 * @uml.property name="min"
+	 */
+	protected double min;
 
-  /**
-   * The maximum so far.
-   * 
-   * @uml.property name="max"
-   */
-  protected double max;
+	/**
+	 * The maximum so far.
+	 * 
+	 * @uml.property name="max"
+	 */
+	protected double max;
 
-  /**
-   * The name of this series.
-   * 
-   * @uml.property name="varName"
-   */
-  protected String varName;
+	/**
+	 * The name of this series.
+	 * 
+	 * @uml.property name="varName"
+	 */
+	protected String varName;
 
-  static Logger logger = Logger.getLogger(CummulativeDistribution.class);
+	static Logger logger = Logger.getLogger(CummulativeDistribution.class);
 
-  public CummulativeDistribution( String varName ) {
-    this.varName = varName;
-    initialise();
-  }
+	public CummulativeDistribution(String varName) {
+		this.varName = varName;
+		initialise();
+	}
 
-  public CummulativeDistribution() {
-    this("");
-  }
+	public CummulativeDistribution() {
+		this("");
+	}
 
-  public void initialise() {
-    n = 0;
-    total = 0;
-    min = Double.POSITIVE_INFINITY;
-    max = Double.NEGATIVE_INFINITY;
-    totalSq = 0;
-  }
+	public void initialise() {
+		n = 0;
+		total = 0;
+		min = Double.POSITIVE_INFINITY;
+		max = Double.NEGATIVE_INFINITY;
+		totalSq = 0;
+	}
 
-  /**
-   * Add a new datum to the series.
-   */
-  public void newData( double i ) {
-    n++;
-    total += i;
-    totalSq += i * i;
-    if ( i > max ) {
-      max = i;
-    }
-    if ( i < min ) {
-      min = i;
-    }
-  }
+	/**
+	 * Add a new datum to the series.
+	 */
+	public void newData(double i) {
+		n++;
+		total += i;
+		totalSq += i * i;
+		if (i > max) {
+			max = i;
+		}
+		if (i < min) {
+			min = i;
+		}
+	}
 
-  /**
-   * Get the number of items in the series.
-   * 
-   * @uml.property name="n"
-   */
-  public int getN() {
-    return n;
-  }
+	/**
+	 * Get the number of items in the series.
+	 * 
+	 * @uml.property name="n"
+	 */
+	public int getN() {
+		return n;
+	}
 
-  /**
-   * Get the mean of the data.
-   */
-  public double getMean() {
-    return total / n;
-  }
+	/**
+	 * Get the mean of the data.
+	 */
+	public double getMean() {
+		return total / n;
+	}
 
-  /**
-   * Get the variance of the data about origin.
-   */
-  public double getVariance( double origin ) {
-    return Math.abs(totalSq / n - origin * origin);
-  }
+	/**
+	 * Get the variance of the data about origin.
+	 */
+	public double getVariance(double origin) {
+		return Math.abs(totalSq / n + (origin - 2 * getMean()) * origin);
+	}
 
-  /**
-   * Get the standard deviation of the data about origin.
-   */
-  public double getStdDev( double origin ) {
-    return Math.sqrt(getVariance(origin));
-  }
+	/**
+	 * Get the standard deviation of the data about origin.
+	 */
+	public double getStdDev(double origin) {
+		return Math.sqrt(getVariance(origin));
+	}
 
-  /**
-   * Get the variance about the mean.
-   */
-  public double getVariance() {
-    return getVariance(getMean());
-  }
+	/**
+	 * Get the variance about the mean.
+	 */
+	public double getVariance() {
+		return getVariance(getMean());
+	}
 
-  /**
-   * Get the standard deviation from the mean.
-   */
-  public double getStdDev() {
-    return getStdDev(getMean());
-  }
+	/**
+	 * Get the standard deviation from the mean.
+	 */
+	public double getStdDev() {
+		return getStdDev(getMean());
+	}
 
-  /**
-   * Get the coefficient of variable about origin.
-   */
-  public double getVarCoef( double origin ) {
-    return 100 * getStdDev(origin) / origin;
-  }
+	/**
+	 * Get the coefficient of variable about origin.
+	 */
+	public double getVarCoef(double origin) {
+		return 100 * getStdDev(origin) / origin;
+	}
 
-  /**
-   * Get the minimum datum.
-   * 
-   * @uml.property name="min"
-   */
-  public double getMin() {
-    return min;
-  }
+	/**
+	 * Get the minimum datum.
+	 * 
+	 * @uml.property name="min"
+	 */
+	public double getMin() {
+		return min;
+	}
 
-  /**
-   * Get the maximum datum.
-   * 
-   * @uml.property name="max"
-   */
-  public double getMax() {
-    return max;
-  }
+	/**
+	 * Get the maximum datum.
+	 * 
+	 * @uml.property name="max"
+	 */
+	public double getMax() {
+		return max;
+	}
 
-  /**
-   * Get the total of the data
-   * 
-   * @uml.property name="total"
-   */
-  public double getTotal() {
-    return total;
-  }
+	/**
+	 * Get the total of the data
+	 * 
+	 * @uml.property name="total"
+	 */
+	public double getTotal() {
+		return total;
+	}
 
-  public Object clone() throws CloneNotSupportedException {
-    return super.clone();
-  }
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
 
-  public void reset() {
-    initialise();
-  }
+	public void reset() {
+		initialise();
+	}
 
-  public String getName() {
-    return varName;
-  }
+	public String getName() {
+		return varName;
+	}
 
-  public String toString() {
-    return "(" + getClass() + " varName:" + varName + " n:" + n + " mean:"
-        + getMean() + " stdev:" + getStdDev() + " min:" + min + " max:" + max
-        + ")";
-  }
+	public String toString() {
+		return "(" + getClass() + " varName:" + varName + " n:" + n + " mean:"
+				+ getMean() + " stdev:" + getStdDev() + " min:" + min + " max:"
+				+ max + ")";
+	}
 
-  public void log() {
-    logger.info(getName());
-    logger.info("\tn:\t" + getN());
-    logger.info("\tmin:\t" + getMin());
-    logger.info("\tmax:\t" + getMax());
-    logger.info("\tmean:\t" + getMean());
-    logger.info("\tstdev:\t" + getStdDev());
-  }
+	public void log() {
+		logger.info(getName());
+		logger.info("\tn:\t" + getN());
+		logger.info("\tmin:\t" + getMin());
+		logger.info("\tmax:\t" + getMax());
+		logger.info("\tmean:\t" + getMean());
+		logger.info("\tstdev:\t" + getStdDev());
+	}
 
-  public void combine( Distribution other ) {
-    CummulativeDistribution d = (CummulativeDistribution) other;
-    min = Math.min(this.min, d.min);
-    max = Math.max(this.max, d.max);
-    n += d.n;
-    total += d.total;
-    totalSq += d.totalSq;
-  }
+	public void combine(Distribution other) {
+		CummulativeDistribution d = (CummulativeDistribution) other;
+		min = Math.min(this.min, d.min);
+		max = Math.max(this.max, d.max);
+		n += d.n;
+		total += d.total;
+		totalSq += d.totalSq;
+	}
 
-  public double getTrimmedMean( double p ) {
-    if ( p > 0 ) {
-      throw new Error("method not implemented for p > 0");
-    } else {
-      return getMean();
-    }
-  }
+	public double getTrimmedMean(double p) {
+		if (p > 0) {
+			throw new Error("method not implemented for p > 0");
+		} else {
+			return getMean();
+		}
+	}
 
 }
