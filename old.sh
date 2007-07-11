ba7f521ffb01b823836e76df18d9b0003af20380
sphelps <sphelps>
2007-07-11 13:57:49 +0000

    *** empty log message ***

--- old.sh
+++
@@ -1,4 +0,0 @@
-export CP=.:/LOCAL/moyaux/workspace/jasa/bin/
-for jar in `ls /LOCAL/moyaux/lib/jar/*.jar`; do export CP=$CP:$jar; done
-echo $CP
-java -classpath $CP uk.ac.liv.auction.RepastMarketSimulation $1
