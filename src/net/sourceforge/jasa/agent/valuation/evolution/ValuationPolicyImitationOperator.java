9e92c5de3612356304a45406aec580563fe3f5df
sphelps <sphelps>
2012-06-09 16:49:20 +0000

    ValuationPolicyImitationOperator correctly rewires event listeners

--- src/net/sourceforge/jasa/agent/valuation/evolution/ValuationPolicyImitationOperator.java
+++ src/net/sourceforge/jasa/agent/valuation/evolution/ValuationPolicyImitationOperator.java
@@ -1,7 +1,9 @@
 package net.sourceforge.jasa.agent.valuation.evolution;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Required;
 
+import net.sourceforge.jabm.EventScheduler;
 import net.sourceforge.jabm.agent.Agent;
 import net.sourceforge.jabm.evolution.ImitationOperator;
 import net.sourceforge.jasa.agent.AbstractTradingAgent;
@@ -14,16 +16,21 @@ public class ValuationPolicyImitationOperator implements ImitationOperator {
 	static Logger logger = Logger.getLogger(ValuationPolicyImitationOperator.class);
 
 	@Override
-	public void inheritBehaviour(Agent child, Agent parent) {
-		if (!(parent instanceof MarketMakerAgent && child instanceof MarketMakerAgent)) {
+	public void inheritBehaviour(Agent i, Agent j) {
+		if (!(i instanceof MarketMakerAgent && j instanceof MarketMakerAgent)) {
 			try {
-				AbstractTradingAgent tradingAgent = (AbstractTradingAgent) parent;
-				ReturnForecastValuationPolicy policy = (ReturnForecastValuationPolicy) tradingAgent
+				AbstractTradingAgent child = (AbstractTradingAgent) i;
+				AbstractTradingAgent parent = (AbstractTradingAgent) j;
+				ReturnForecastValuationPolicy oldPolicy = (ReturnForecastValuationPolicy) child
+						.getValuationPolicy();
+				ReturnForecastValuationPolicy policy = 
+						(ReturnForecastValuationPolicy) parent
 						.getValuationPolicy();
 				ReturnForecastValuationPolicy newPolicy;
 				newPolicy = (ReturnForecastValuationPolicy) policy.clone();
-				((AbstractTradingAgent) child).setValuationPolicy(newPolicy);
-				newPolicy.setAgent((TradingAgent) child);
+				child.setValuationPolicy(newPolicy);
+				oldPolicy.dispose();
+				newPolicy.setAgent(child);
 				if (logger.isDebugEnabled())
 					logger.debug("Inheriting " + newPolicy + " for " + child);
 
