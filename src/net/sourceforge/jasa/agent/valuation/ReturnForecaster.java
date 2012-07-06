2d78281ad60c9e44f3586799741c1d4113cf833f
sphelps <sphelps>
2012-07-06 16:47:14 +0000

    *** empty log message ***

--- src/net/sourceforge/jasa/agent/valuation/ReturnForecaster.java
+++ src/net/sourceforge/jasa/agent/valuation/ReturnForecaster.java
@@ -4,12 +4,13 @@ import net.sourceforge.jabm.event.EventListener;
 import net.sourceforge.jabm.event.EventSubscriber;
 import net.sourceforge.jasa.market.Market;
 
-public interface ReturnForecaster extends EventListener, EventSubscriber, Cloneable {
+public interface ReturnForecaster extends EventListener, EventSubscriber,
+		Cloneable {
 
 	public double getReturnForecast(Market market);
-	
+
 	public Object clone() throws CloneNotSupportedException;
-	
+
 	public void dispose();
-	
+
 }
