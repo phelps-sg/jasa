f79751417384b9a06c8774b9b16e3af833d4c4e5
sphelps <sphelps>
2015-01-28 17:07:25 +0000

    added tests for walking the book and partial fills

--- src/test/java/net/sourceforge/jasa/market/FourHeapTest.java
+++ src/test/java/net/sourceforge/jasa/market/FourHeapTest.java
@@ -144,6 +144,116 @@ public class FourHeapTest extends TestCase {
 		}
 	}
 
+	/**
+	 * Test that a buy order can be matched
+	 * with a sell order of lower volume, resulting
+	 * in the remaining volume being retained on the book.
+	 */
+	public void testPartialBuyFills() {
+		try {
+
+			TradingAgent trader1 = new MockTrader(this, 0, 0, auction);
+			TradingAgent trader2 = new MockTrader(this, 0, 0, auction);
+			Order buy = new Order(trader1, 10, 10.0, true);
+			Order sell = new Order(trader2, 5, 5.0, false);
+			shoutEngine.add(buy);
+			shoutEngine.add(sell);
+
+			List<Order> matched = shoutEngine.matchOrders();
+			System.out.println(matched);
+
+			assertTrue(matched.contains(sell));
+
+			// Order should remain on the book with the outstanding volume
+			assertTrue(buy.getQuantity() == 5);
+			assertTrue(shoutEngine.bOut.contains(buy));
+
+		} catch (DuplicateShoutException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+
+	/**
+	 * Test that a sell order can be matched
+	 * with a buy order of lower volume, resulting
+	 * in the remaining volume being retained on the book.
+	 */
+	
+	public void testPartialSellFills() {
+		try {
+			TradingAgent trader1 = new MockTrader(this, 0, 0, auction);
+			TradingAgent trader2 = new MockTrader(this, 0, 0, auction);
+			Order buy = new Order(trader1, 5, 10.0, true);
+			Order sell = new Order(trader2, 10, 5.0, false);
+			shoutEngine.add(buy);
+			shoutEngine.add(sell);
+			List<Order> matched = shoutEngine.matchOrders();
+			System.out.println(matched);
+//			assertTrue(matched.contains(buy));
+			assertTrue(matched.contains(sell));
+			
+			// Order should remain on the book with the oustanding volume
+			assertTrue(sell.getQuantity() == 5);
+			assertTrue(shoutEngine.sOut.contains(sell));
+
+		} catch (DuplicateShoutException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+	
+	public void testWalkingBookBuy() {
+		try {
+			// Create two test traders
+			TradingAgent trader1 = new MockTrader(this, 0, 0, auction);
+			TradingAgent trader2 = new MockTrader(this, 0, 0, auction);
+			
+			// The total volume of buy orders
+			int buyQuantity = 20;
+			
+			// The volume of the sell orders
+			int sell1Quantity = 10;
+			int sell2Quantity = 8;
+			
+			// The total tradable volume
+			int tradableVolume = sell1Quantity + sell2Quantity;
+			int untradedVolume = buyQuantity - tradableVolume;
+
+			// Create the limit-orders
+			Order buy = new Order(trader1, buyQuantity, 10.0, true);
+			Order sell1 = new Order(trader2, sell1Quantity, 5.0, false);
+			Order sell2 = new Order(trader2, sell2Quantity, 5.5, false);
+
+			// Submit them to the exchange
+			shoutEngine.add(buy);
+			shoutEngine.add(sell1);
+			shoutEngine.add(sell2);
+			
+			// Uncross the market
+			List<Order> matched = shoutEngine.matchOrders();
+			System.out.println(matched);
+
+			// The total volume of uncrossed orders should be 18
+			// We multiply by two because we count both bids and asks
+			assertTrue(totalVolume(matched) == tradableVolume * 2);
+			
+			// The untraded volume is held in the child of the original order
+			Order remainingOrder = buy.getChild();
+			assertTrue(remainingOrder.getQuantity() == untradedVolume);
+
+			// An order with this remaining volume should remain on the book
+			assertTrue(shoutEngine.bOut.contains(remainingOrder));
+
+			// There should be no remaining volume on the ask side
+			assertTrue(shoutEngine.sOut.isEmpty());
+
+		} catch (DuplicateShoutException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+
 	public void testRandom() {
 
 		int matches = 0;
@@ -220,6 +330,14 @@ public class FourHeapTest extends TestCase {
 
 		System.out.println("Matches = " + matches);
 
+	}
+	
+	public int totalVolume(List<Order> orders) {
+		int result = 0;
+		for(Order order : orders) {
+			result += order.getQuantity();
+		}
+		return result;
 	}
 
 	public static void main(String[] args) {
