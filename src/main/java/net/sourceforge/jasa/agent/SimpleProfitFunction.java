78c12a15a6b806930586b037956cc2fc45072601
sphelps <sphelps>
2016-01-21 13:35:47 +0000

    serialization changes and moved to new snapshot version

--- src/main/java/net/sourceforge/jasa/agent/SimpleProfitFunction.java
+++ src/main/java/net/sourceforge/jasa/agent/SimpleProfitFunction.java
@@ -1,5 +1,7 @@
 package net.sourceforge.jasa.agent;
 
+import java.io.Serializable;
+
 import net.sourceforge.jabm.agent.Agent;
 import net.sourceforge.jasa.agent.ProfitFunction;
 import net.sourceforge.jasa.market.Market;
@@ -7,7 +9,7 @@ import net.sourceforge.jasa.market.Market;
 /**
  * (C) Steve Phelps 2015
  */
-public class SimpleProfitFunction implements ProfitFunction {
+public class SimpleProfitFunction implements ProfitFunction, Serializable {
 
     @Override
     public double calculateProfit(AbstractTradingAgent agent, Market auction, int quantity, double price) {
