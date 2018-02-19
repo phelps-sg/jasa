56051bb6f1ec869e9852fc196f7dc98c758d6c5f
phelps-sg <sphelps@sphelps.net>
2018-02-19 17:13:24 +0000

    - simplified internal representation - Price now extends Number

--- src/main/java/net/sourceforge/jasa/market/Price.java
+++ src/main/java/net/sourceforge/jasa/market/Price.java
@@ -1,44 +1,53 @@
 package net.sourceforge.jasa.market;
 
 import java.math.BigDecimal;
+import java.lang.Math;
 import java.text.DecimalFormat;
 
-public class Price implements Comparable<Price> {
+public class Price extends java.lang.Number implements Comparable<Price> {
 
     public static final int DEFAULT_EXPONENT = 4;
 
-    protected long whole;
+    protected long longValue;
 
-    protected long fractional;
+//    protected long fractional;
 
-    protected int exponent;
+//    protected int exponent;
 
     protected int multiplier;
 
 	static DecimalFormat currencyFormatter = new DecimalFormat(
 	    "+#########0.0000;-#########.0000");
 
-    public Price(long whole, long fractional, int exponent) {
-        this.whole = whole;
-        this.fractional = fractional;
-        this.exponent = exponent;
-        this.multiplier = 1;
+	protected Price(int exponent) {
+	    this.multiplier = 1;
         for(int i=0; i<exponent; i++) {
             multiplier *= 10;
         }
     }
 
-    public Price(long whole, long fractional) {
-        this(whole, fractional, DEFAULT_EXPONENT);
+    public Price(long longValue, int exponent) {
+	    this(exponent);
+        this.longValue = longValue;
+//        this.exponent = exponent;
     }
 
-    public Price(long whole, int exponent) {
-        this(whole, 0, exponent);
+    public Price(long whole, long fractional, int exponent) {
+	   this(exponent);
+	   this.longValue = whole * multiplier + fractional;
     }
+
+    public Price(long longValue) {
+        this(longValue, DEFAULT_EXPONENT);
+    }
+
+//    public Price(long longValue, int exponent) {
+//        this(longValue, 0, exponent);
+//    }
 
     public Price(double price, int exponent) {
-        this((long) price, exponent);
-        this.fractional = (long) (Math.abs(price - whole) * multiplier);
+	    this(exponent);
+	    this.longValue = (long) (price * multiplier);
     }
 
     public Price(double price) {
@@ -46,59 +55,72 @@ public class Price implements Comparable<Price> {
     }
 
     public Price(BigDecimal price, int exponent) {
-        this(price.intValue(), exponent);
-        this.fractional = price.multiply(new BigDecimal(multiplier)).longValue();
+        this(exponent);
+        this.longValue = price.multiply(new BigDecimal(multiplier)).longValue();
     }
 
     public Price(BigDecimal price) {
         this(price, DEFAULT_EXPONENT);
     }
 
+    @Override
+    public int intValue() {
+        return (int) longValue;
+    }
+
     public long longValue() {
-        return whole * multiplier + fractional;
+        return longValue;
+    }
+
+    @Override
+    public float floatValue() {
+        return (float) longValue;
     }
 
     public double doubleValue() {
-        return whole + ((double) fractional) / multiplier;
+        return ((double) longValue) / multiplier;
     }
 
     @Override
     public int compareTo(Price other) {
-        if (this.exponent != other.exponent) {
+        if (this.multiplier != other.multiplier) {
             throw new UnsupportedOperationException("Comparision of prices with different tick sizes is not supported.");
         }
         return (int) (other.longValue() - this.longValue());
     }
 
     public boolean isPositive() {
-        return whole > 0;
+        return longValue > 0;
     }
 
     public boolean isNegative() {
-        return whole < 0;
+        return longValue < 0;
     }
 
     public String toPrettyString() {
         return currencyFormatter.format(doubleValue());
     }
 
-    @Override
-    public String toString() {
-        return "Price{" +
-                "whole=" + whole +
-                ", fractional=" + fractional +
-                ", exponent=" + exponent +
-                ", multiplier=" + multiplier +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "Price{" +
+//                "whole=" + whole +
+//                ", fractional=" + fractional +
+//                ", multiplier=" + multiplier +
+//                '}';
+//    }
 
     public static void main(String[] args) {
-        Price p = new Price(3, 14L);
+        Price p = new Price(3.14);
         System.out.println(p);
         System.out.println(p.toPrettyString());
 
         Price p2 = new Price(3.19);
         System.out.println(p2);
         System.out.println(p2.toPrettyString());
+
+        Price p3 = new Price(3.19, 2);
+        System.out.println(p3);
+        System.out.println(p3.toPrettyString());
     }
 }
